5fb6a48b36258890049473e13f43d363b8858f23
Alba G <a.garciarodriguez@informatica-medica.com>
2023-04-25 15:39:19 +0200

    Folder Rename

--- 04_DAY5/00_AverageHeight/main.py
+++
@@ -1,7 +0,0 @@
-student_heights = [ 180 , 124 , 165 , 173 , 189 , 169 , 146 ]
-
-def getAverage():
-    average = sum( student_heights ) / len( student_heights )
-    return round( average )
-
-print( getAverage() )
